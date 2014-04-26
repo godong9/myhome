5d2a3f8be81bc59c1e849d97d9d602f9a0cb0667
Go Dong Hyun <godong9@gmail.com>
2014-04-26 23:55:51 +0900

    home page init2

---
+++ app/scripts/controllers/resume.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('myhomeApp')
+  .controller('ResumeCtrl', function ($scope, $http) {
+    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+  });
