b2e168fdf7f77629eb95f056df58bd88d75750e1
Go Dong Hyun <godong9@gmail.com>
2014-05-10 14:00:13 +0900

    modify

--- app/scripts/controllers/resume.js
+++ app/scripts/controllers/resume.js
@@ -3,12 +3,16 @@
 angular.module('myhomeApp')
   .controller('ResumeCtrl', function ($scope, $http, $compile) {
     $http.get('/api/resume').success(function(resumeData) {
-      console.log(resumeData);
 
-      resumeData.profile = resumeData.profile.replace(/\n/gi, '<br>');
+      resumeData.profile = new String(resumeData.profile);
+      resumeData.profile.replace(/\n/gi, '<br>');
+      resumeData.skills = new String(resumeData.skills);
       resumeData.skills = resumeData.skills.replace(/\n/gi, '<br>');
+      resumeData.experience = new String(resumeData.experience);
       resumeData.experience = resumeData.experience.replace(/\n/gi, '<br>');
+      resumeData.history = new String(resumeData.history);
       resumeData.history = resumeData.history.replace(/\n/gi, '<br>');
+      resumeData.education = new String(resumeData.education);
       resumeData.education = resumeData.education.replace(/\n/gi, '<br>');
       $scope.resumeData = resumeData;
     });
